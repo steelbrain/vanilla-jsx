15f7c20bc47299ae8ad0cc9c06fbd1a3aa3017e2
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-14 23:54:55 +0500

    :new: Add base for new structure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,56 @@
-'use strict'
+'use babel';
 
+(function() {
+  'use strict'
+
+  class Component {
+    constructor(tagName, renderView) {
+      this.element = null
+    }
+    render() {
+
+    }
+    renderAsString() {
+
+    }
+    dispose() {
+      this.element = null
+    }
+  }
+
+  export function component(component) {
+    if (typeof component !== 'object') {
+      throw new Error('Invalid component params provided')
+    }
+    if (typeof component.tagName !== 'string') {
+      throw new Error('tagName must be string')
+    }
+    if (typeof component.renderView !== 'function') {
+      throw new Error('renderView must be a function')
+    }
+    const currentComponent = Object.create(Component)
+    for (let key in component) {
+      if (key === 'tagName') {
+        // No-Op
+      } else if (key === 'render') {
+        currentComponent['renderView'] = component[key]
+      } else if (currentComponent[key]) {
+        throw new Error(`Key '${key}' not allowed in component`)
+      } else currentComponent[key] = component[key]
+    }
+    return new currentComponent(component.tagName)
+  }
+
+  export function jsx(name, attributes, ...children) {
+    return {name, attributes, children}
+  }
+
+  if (typeof module !== 'undefined') {
+    module.exports.component = component
+    module.exports.jsx = jsx
+  }
+
+})()
 function jsx(name, attributes, ...children) {
   const element = document.createElement(name)
 
